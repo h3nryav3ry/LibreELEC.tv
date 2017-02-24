761f3aa2f22fb481897b166d1af069b380057afd
Radostan Riedel <raybuntu@googlemail.com>
2017-02-24 08:27:06 +0100

    kodi: update to 8b422f74

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="d2417c7"
+PKG_VERSION="8b422f7"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
