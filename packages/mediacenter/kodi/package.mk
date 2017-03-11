a72f2a86a3377ad3fd4eafe9d942faf529981050
Radostan Riedel <raybuntu@googlemail.com>
2017-03-11 08:41:06 +0100

    kodi: update to c6d0bb5

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="5d5bd01"
+PKG_VERSION="c6d0bb5"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
