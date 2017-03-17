35da1c1bb3743e520f96a3a31951c720264e6d4d
Radostan Riedel <raybuntu@googlemail.com>
2017-03-17 07:54:14 +0100

    kodi: update to 5814568

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="c6d0bb5"
+PKG_VERSION="5814568"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
